9618c096e4df2ff1051cd8c45ade3dc0e92b883e
harngalf <harngalf@hotmail.com>
2022-04-28 04:24:55 -0500

    errores connecion base de dats deploy

--- src/database.py
+++ src/database.py
@@ -30,11 +30,11 @@ try:
         DATABASE_URL
     )
 
-    # SessionLocal = sessionmaker(
-    #     autocommit=False,
-    #     autoflush=False,
-    #     bind=engine
-    # )
+    SessionLocal = sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine
+    )
     Base = declarative_base()
     
     #meta = MetaData
