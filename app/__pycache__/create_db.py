7c6f1729064c971ada82f3adcfe3a1bcd6ca60b2
harngalf <harngalf@hotmail.com>
2022-05-08 13:58:25 -0500

    deploy correcto código refacrtorizado

--- app/__pycache__/create_db.py
+++
@@ -1,7 +0,0 @@
-
-from src.database import Base, engine
-from models import movies_mod, user_mod, user_movies_mod
-
-print("creating database")
-
-Base.metadata.create_all(engine)
--- create_db.py
+++ create_db.py
@@ -1,5 +1,5 @@
 
-from src.database import Base, engine
+from database import Base, engine
 from models import movies_mod, user_mod, user_movies_mod
 
 print("creating database")
