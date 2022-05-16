6b12a6d9a2a81e00387c058836a98d88409c586a
harngalf <harngalf@hotmail.com>
2022-05-15 19:34:29 -0500

    repair database file connection

--- database.py
+++ database.py
@@ -4,60 +4,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL = "postgresql://harngalf:A142536@postgres/std_ghibli_db"
-#SQLALCHEMY_DATABASE_URL = "postgresql://harngalf:A142536@localhost:5432/std_ghibli_db"
-#
-SQLALCHEMY_DATABASE_URL = "postgresql://yayrtajuzkmisy:781072834cdacb0f7e4b63106f0b84907ba9ed57076a8f99876be2db1f8d98d7@ec2-34-194-73-236.compute-1.amazonaws.com:5432/dan4kvev3lephd"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-    #DATABASE_URL
+try:
+    # Connect to the database
+    
+    #SQLALCHEMY_DATABASE_URL = "postgresql://harngalf:A142536@localhost:5432/std_ghibli_db"
+    SQLALCHEMY_DATABASE_URL = "postgresql://yayrtajuzkmisy:781072834cdacb0f7e4b63106f0b84907ba9ed57076a8f99876be2db1f8d98d7@ec2-34-194-73-236.compute-1.amazonaws.com:5432/dan4kvev3lephd"
+
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL
     )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
+    SessionLocal = sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine
     )
-Base = declarative_base()
+    
+    Base = declarative_base()
     
     
-
-
-# try:
-#     # Connect to the database
+    print("Connection to database successful")
     
-#     #SQLALCHEMY_DATABASE_URL = "postgresql://harngalf:A142536@postgres/std_ghibli_db"
-#     #SQLALCHEMY_DATABASE_URL = "postgresql://harngalf:A142536@localhost:5432/std_ghibli_db"
-#     #DATABASE_URL = "postgresql://harngalf:A142536@localhost:5432/std_ghibli_db"
-#     DATABASE_URL = 'postgres://yayrtajuzkmisy:781072834cdacb0f7e4b63106f0b84907ba9ed57076a8f99876be2db1f8d98d7@ec2-34-194-73-236.compute-1.amazonaws.com:5432/dan4kvev3lephd'
-
-#     #database = databases.Database(DATABASE_URL)
-
-#     #metadata = sqlalchemy.MetaData()
-
-#     #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-#     engine = create_engine(
-#         #SQLALCHEMY_DATABASE_URL
-#         DATABASE_URL
-#     )
-
-#     SessionLocal = sessionmaker(
-#         autocommit=False,
-#         autoflush=False,
-#         bind=engine
-#     )
-#     Base = declarative_base()
-#     #metadata.create_all(engine)
-    
-#     #meta = MetaData
-
-#     #conn = engine.connect()
-    
-    
-#     print("Connection to database successful")
-    
-# except Exception as ex:
-#     print(ex)
+except Exception as ex:
+    print(ex)
 
